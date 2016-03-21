039d9f14493dbe43f687c6c59011772f3ea97f8d
Sayak Biswas <sayak90@gmail.com>
2016-03-21 19:59:19 -0400

    Changelog: 1. Fixed issue with Raycast not going in proper direction from CardboardHead.

--- Assets/Scripts/DreamSequence.cs
+++ Assets/Scripts/DreamSequence.cs
@@ -2,6 +2,10 @@
 using System.Collections;
 
 public class DreamSequence : MonoBehaviour {
+	private float waitBeforeSequenceStarts = 1.2f;
+	private bool isDreamStartAudioPlayed = false;
+	public LayerMask layerMask;
+	public GameObject moon;
 
 	// Use this for initialization
 	void Start () {
@@ -10,6 +14,27 @@ public class DreamSequence : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		RaycastHit hitInfo;
+		if (waitBeforeSequenceStarts > 0) {
+			waitBeforeSequenceStarts -= Time.deltaTime;
+		} else {
+			if (!isDreamStartAudioPlayed) {
+				Debug.Log ("Whoa! Where am I? Is this a dream?"); //TODO: Play audio here.
+				isDreamStartAudioPlayed = true;
+			}
+		}
+
+		if (isDreamStartAudioPlayed) {
+			//Transform Moon
+			Vector3 moonDirection = moon.transform.position - transform.position;
+			if(Physics.Raycast(Cardboard.SDK.GetComponentInChildren<CardboardHead> ().Gaze, out hitInfo, 99999, layerMask)) {
+				Debug.Log ("Raycast hit :: " + hitInfo.transform.gameObject.name);
+				Vector3 lookDirection = Cardboard.SDK.GetComponentInChildren<CardboardHead> ().Gaze.direction;
+				float moonAngle = Vector3.Angle (moonDirection, lookDirection);
+				if (moonAngle <= 5.0f) {
+					Debug.Log ("The moon!");
+				}
+			}
+		}
 	}
 }
